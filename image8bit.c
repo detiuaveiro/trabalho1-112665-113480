a10d92aeee1da8f949a6c177220c5ddba6a9f284
alof04 <hpclafonso@gmail.com>
2023-11-26 17:01:24 +0000

    apagar oldImageBlur - last commit

--- image8bit.c
+++ image8bit.c
@@ -594,44 +594,6 @@ int ImageLocateSubImage(Image img1, int* px, int* py, Image img2) {
 /// Each pixel is substituted by the mean of the pixels in the rectangle
 /// [x-dx, x+dx]x[y-dy, y+dy].
 /// The image is changed in-place.
-void OldImageBlur(Image img, int dx, int dy) {
-  assert (img != NULL);
-  assert (dx >= 0 && dy >= 0);
-
-  // Create a temporary image to store the blurred result
-  Image blurredImage = ImageCreate(img->width+1, img->height+1, img->maxval);
-  ImagePaste(blurredImage,0, 0, img);
-  for (int i = 0; i <= img->width; i++) {
-    for (int j = 0; j <= img->height; j++) {
-      int sum = 0;
-      int count = 0;
-
-      for (int k = i - dx; k <= i + dx; k++) {
-        for (int l = j - dy; l <= j + dy; l++) {
-          ITERATIONS++;
-          if (ImageValidPos(img, k, l)) {
-            sum += ImageGetPixel(img, k, l);
-            count++;
-          }
-        }
-      }
-      ImageSetPixel(blurredImage, i, j, (sum + count / 2) / count);
-    }
-  }
-    for (int i = 0; i < img->width; i++) {
-    for (int j = 0; j < img->height; j++) {
-      ImageSetPixel(img, i, j, ImageGetPixel(blurredImage, i, j));
-      ITERATIONS++;
-    }
-  }
-  ImageDestroy(&blurredImage); //destrói a imagem
-}
-
-///filtering
-/// Blur an image by a applying a (2dx+1)x(2dy+1) mean filter.
-/// Each pixel is substituted by the mean of the pixels in the rectangle
-/// [x-dx, x+dx]x[y-dy, y+dy].
-/// The image is changed in-place.
 void ImageBlur(Image img, int dx, int dy){
   int* sumPixels; //array onde iremos armazenar a soma de pixeis
   int blurValue, xstart, xend, ystart, yend, xSize, ySize, count; 
